b68886eb2c85d1ce94c0aef82e168a66f8531799
nyxmmv <kaceyobiajulu@gmail.com>
2026-08-29 18:01:33 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- telemedicine_project/settings.py
+++ telemedicine_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l-oll4b2p!672sxqs*$7m#kf9^o(%9v9o43jjzxso^nl_l=8=$'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l-oll4b2p!672sxqs*$7m#kf9^o(%9v9o43jjzxso^nl_l=8=$')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['telemedicine-fzjs.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
